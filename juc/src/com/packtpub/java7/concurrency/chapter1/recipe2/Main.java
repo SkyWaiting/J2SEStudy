4832c957225ca59ade11e9f9dd353ba70d0d3e02
guorui <guorui016@gmail.com>
2013-11-16 11:19:19 +0800

    ok

--- juc/src/com/packtpub/java7/concurrency/chapter1/recipe2/Main.java
+++ juc/src/com/packtpub/java7/concurrency/chapter1/recipe2/Main.java
@@ -13,6 +13,7 @@ public class Main {
         //Thread priority information
         System.out.printf("Minimum Priority: %s\n", Thread.MIN_PRIORITY);
 
+
         Thread threads[] = new Thread[10];
         Thread.State status[] = new Thread.State[10];
         for (int i=0; i<10; i++){
